9d20181b4938a6ba194eee430550917da31812bb
Muhammad Yoga Ibrahim <yogaibhh@users.noreply.github.com>
2026-06-15 23:09:44 +0700

    Configure base path and routing for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import DashboardDetail from './pages/DashboardDetail'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard/:id" element={<DashboardDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
